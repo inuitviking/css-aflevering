d3a0ab3cad1453da1807753adb55627053600248
inuitviking <runurth@gmail.com>
2019-05-17 23:38:46 +0200

    Added new pages (without content), modified styles

---
+++ public_html/pages/404.php
@@ -0,0 +1,8 @@
+<main>
+	<section>
+		<h2>404: Not found</h2>
+		<p>
+			The url you visisted doesn't exist. You can go to the <a href="home">home page</a> or click on one of navigational links.
+		</p>
+	</section>
+</main>
